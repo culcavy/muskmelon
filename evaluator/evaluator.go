7cbc71f54976d782b165b62192d34333eb7e833e
hollykbuck <101749900+hollykbuck@users.noreply.github.com>
2022-10-12 21:34:04 +0800

    添加对前缀表达式 eval 的支持

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -6,25 +6,56 @@ import (
 )
 
 var (
+	NULL  = &object.Null{}
 	TRUE  = &object.Boolean{Value: true}
 	FALSE = &object.Boolean{Value: false}
 )
 
 // Eval eval 传入的 ast 节点
 func Eval(node ast.Node) object.Object {
-	switch node := node.(type) {
-	// Statements
+	switch nodeType := node.(type) {
 	case *ast.Program:
-		return evalStatements(node.Statements)
+		// Statements
+		return evalStatements(nodeType.Statements)
 	case *ast.ExpressionStatement:
-		return Eval(node.Expression)
-		// Expressions
+		// Statements
+		return Eval(nodeType.Expression)
 	case *ast.IntegerLiteral:
-		return &object.Integer{Value: node.Value}
+		// Expressions
+		return &object.Integer{Value: nodeType.Value}
 	case *ast.Boolean:
-		return nativeBoolToBooleanObject(node.Value)
+		// Expressions
+		return nativeBoolToBooleanObject(nodeType.Value)
+	case *ast.PrefixExpression:
+		// Expressions
+		right := Eval(nodeType.Right)
+		return evalPrefixExpression(nodeType.Operator, right)
 	}
 	return nil
+}
+
+// evalPrefixExpression eval 前缀表达式. 根据 operator 类型将实现委托给具体的 eval 函数.
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+// evalBangOperatorExpression eval 取反表达式
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
 }
 
 func nativeBoolToBooleanObject(input bool) object.Object {
