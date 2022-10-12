4d4cd35d68a925c26da4964af86890cf08c578e9
hollykbuck <101749900+hollykbuck@users.noreply.github.com>
2022-10-12 21:18:45 +0800

    完善 Eval 函数的实现. 完成对简单 statement 的执行.

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -8,8 +8,23 @@ import (
 // Eval eval 传入的 ast 节点
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
+	// Statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+		// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	}
 	return nil
+}
+
+// evalStatements 批量解释 statement. 将实现委托给 Eval
+func evalStatements(statements []ast.Statement) object.Object {
+	var result object.Object
+	for _, statement := range statements {
+		result = Eval(statement)
+	}
+	return result
 }
