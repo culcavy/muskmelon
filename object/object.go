bb9522eb685310895e0a96802a71436b9637b863
hollykbuck <101749900+hollykbuck@users.noreply.github.com>
2022-10-12 21:11:10 +0800

    添加 Bool 类型,实现 Object 接口

--- object/object.go
+++ object/object.go
@@ -6,15 +6,26 @@ type ObjectType string
 
 const (
 	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
 )
 
+// Object 所有的对象的父类型
 type Object interface {
 	Type() ObjectType
 	Inspect() string
 }
+
+// Integer 实现 Object 接口的整型类型
 type Integer struct {
 	Value int64
 }
 
 func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
