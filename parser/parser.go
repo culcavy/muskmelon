812bcd0208f305268021edabc5e113b9d6f93ea6
hollykbuck <101749900+hollykbuck@users.noreply.github.com>
2022-10-12 18:43:06 +0800

    实现对 call expression 的解析

--- parser/parser.go
+++ parser/parser.go
@@ -72,6 +72,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerInfix(token.NEQ, p.parseInfixExpression)
 	p.registerInfix(token.LT, p.parseInfixExpression)
 	p.registerInfix(token.GT, p.parseInfixExpression)
+	p.registerInfix(token.LPAREN, p.parseCallExpression)
 	p.nextToken()
 	p.nextToken()
 	return p
@@ -414,4 +415,29 @@ func (p *Parser) parseFunctionParameters() (identifiers []*ast.Identifier) {
 		return nil
 	}
 	return
+}
+
+func (p *Parser) parseCallExpression(expression ast.Expression) ast.Expression {
+	exp := &ast.CallExpression{Token: p.curToken, Function: expression}
+	exp.Arguments = p.parseCallArguments()
+	return exp
+}
+
+func (p *Parser) parseCallArguments() []ast.Expression {
+	var args []ast.Expression
+	if p.peekTokenIs(token.RPAREN) {
+		p.nextToken()
+		return args
+	}
+	p.nextToken()
+	args = append(args, p.parseExpression(LOWEST))
+	for p.peekTokenIs(token.COMMA) {
+		p.nextToken()
+		p.nextToken()
+		args = append(args, p.parseExpression(LOWEST))
+	}
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+	return args
 }
