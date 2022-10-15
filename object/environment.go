7aa403a74c2fa6a3e83ef845252dbd70855ac27d
hollykbuck <101749900+hollykbuck@users.noreply.github.com>
2022-10-15 10:02:51 +0800

    实现函数调用
    完善闭包支持

--- object/environment.go
+++ object/environment.go
@@ -1,19 +1,30 @@
 package object
 
+// NewEnclosedEnvironment 创建闭包
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
+}
+
 // NewEnvironment Environment 的构造函数
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
-	return &Environment{store: s}
+	return &Environment{store: s, outer: nil}
 }
 
 // Environment 存放上下文。本质是个 hashmap
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 // Get 从 hashmap 中取数据
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
